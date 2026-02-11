274ff13b93c0078293f7b0883c3f6174177556cb
jaa <farezaahmadk@gmail.com>
2026-02-11 20:05:05 +0800

    Create main.py
    Make shuffle and card_dealt function

---
+++ main.py
@@ -0,0 +1,24 @@
+import random
+
+cards =[]
+suits = ["spades", "clubs", "hearts", "diamonds"] 
+ranks = ["A","2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+for suit in suits:
+    for rank in ranks:
+        cards.append([suit, rank])
+
+def shuffle():
+    random.shuffle(cards)
+
+def deal(number):
+    cards_dealt = []
+    card = cards.pop()
+    return cards_dealt
+
+shuffle()
+cards_dealt = deal(2)
+print(cards_dealt)
+
+
+
+
